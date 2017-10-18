3ba8f1595bd8e67cac89c28a590202713163b23e
unknown <mazur.nadiia@gmail.com>
2017-10-18 16:56:17 +0300

    readme added, test.py: import of EmployeeType added

--- test.py
+++ test.py
@@ -1,31 +1,32 @@
 import unittest
 import EmployeeType
 from Employee import Employee
+from EmployeeType import EmployeeType
 
 class TestEmployee(unittest.TestCase):
 
     def test_fulltime_positive(self):
-        employee = Employee.factory(Employee, "Fred Johnson", EmployeeType.EmployeeType.FULLTIME, 2.5)
+        employee = Employee.factory(Employee, "Fred Johnson", EmployeeType.FULLTIME, 2.5)
         self.assertEqual(employee.calculate_vacation(), 10)
         employee.print_employee()
 
     def test_fulltime_negative(self):
-        employee = Employee.factory(Employee, "Eddie Mercury", EmployeeType.EmployeeType.FULLTIME, -4)
+        employee = Employee.factory(Employee, "Eddie Mercury", EmployeeType.FULLTIME, -4)
         self.assertEqual(employee.calculate_vacation(), None)
         employee.print_employee()
 
     def test_fulltime_zero(self):
-        employee = Employee.factory(Employee, "Capitan Morgan", EmployeeType.EmployeeType.FULLTIME, 0)
+        employee = Employee.factory(Employee, "Capitan Morgan", EmployeeType.FULLTIME, 0)
         self.assertEqual(employee.calculate_vacation(), 0)
         employee.print_employee()
 
     def test_contractor(self):
-        employee = Employee.factory(Employee, "Jane James", EmployeeType.EmployeeType.CONTRACTOR, 0.8)
+        employee = Employee.factory(Employee, "Jane James", EmployeeType.CONTRACTOR, 0.8)
         self.assertEqual(employee.calculate_vacation(), None)
         employee.print_employee()
 
     def test_temporary(self):
-        employee = Employee.factory(Employee, "Laura Ashley", EmployeeType.EmployeeType.TEMPORARY, 3.5)
+        employee = Employee.factory(Employee, "Laura Ashley", EmployeeType.TEMPORARY, 3.5)
         self.assertEqual(employee.calculate_vacation(), None)
         employee.print_employee()
 
